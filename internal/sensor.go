116cb9b10e780e839a5b5777b2a8b86b698c8335
21 <victorsouzam74@gmail.com>
2026-03-16 22:29:50 -0300

    define idle core default values

--- internal/sensor.go
+++ internal/sensor.go
@@ -20,3 +20,18 @@ type CoreSensor struct {
 	MinVoltage 		float64
 	MaxVoltage 		float64
 }
+
+func NewCoreSensor(id int) *CoreSensor {
+	return &CoreSensor {
+		ID: 			id,
+		Temperature:	35.0,
+		Voltage: 		0.9,
+		Utilization:	0.0,
+		Frequency:		3.600,
+
+		BaseFrequency:	3.600,
+		MaxFrequency:	5.200,
+		MinVoltage:		0.9,
+		MaxVoltage:		1.3
+	}
+}
