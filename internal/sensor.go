727dbf54cb616cebe9ead4c62b10854b3f4bca17
21 <victorsouzam74@gmail.com>
2026-03-16 23:27:39 -0300

    add core utilization generation

--- internal/sensor.go
+++ internal/sensor.go
@@ -2,36 +2,54 @@ package processor
 
 import (
 	"math/rand"
-	"time"
+	//"time"
 	//"pkg/protocol"
-	"fmt"
+	//"fmt"
 )
 
 type CoreSensor struct {
+	ID          int
+	Temperature float64
+	Voltage     float64
+	Utilization float64
+	Frequency   float64
 
-	ID 				int
-	Temperature 	float64
-	Voltage 		float64
-	Utilization 	float64
-	Frequency 		float64
-
-	BaseFrequency 	float64
-	MaxFrequency 	float64
-	MinVoltage 		float64
-	MaxVoltage 		float64
+	BaseFrequency float64
+	MaxFrequency  float64
+	MinVoltage    float64
+	MaxVoltage    float64
 }
 
 func NewCoreSensor(id int) *CoreSensor {
-	return &CoreSensor {
-		ID: 			id,
-		Temperature:	35.0,
-		Voltage: 		0.9,
-		Utilization:	0.0,
-		Frequency:		3.600,
+	return &CoreSensor{
+		ID:          id,
+		Temperature: 	35.0,
+		Voltage:     	0.9,
+		Utilization: 	0.0,
+		Frequency:   	3.600,
 
-		BaseFrequency:	3.600,
-		MaxFrequency:	5.200,
-		MinVoltage:		0.9,
-		MaxVoltage:		1.3
+		BaseFrequency: 	3.600,
+		MaxFrequency:  	5.200,
+		MinVoltage:    	0.9,
+		MaxVoltage:    	1.3,
 	}
+}
+
+func (c *CoreSensor) tickUpdate() {
+
+	c.Utilization += (rand.Float64() * 10) - 5
+
+	if c.Utilization > 100.0 {
+		c.Utilization = 100.0
+	}
+
+	if c.Utilization < 0.0 {
+		c.Utilization = 0.0
+	}
+
+	// Temperature
+
+	// Voltage
+
+	// Frequency
 }
