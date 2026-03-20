452c92b2827d5070e4f864ce7ea9cfd3f28b4339
21 <victorsouzam74@gmail.com>
2026-03-20 12:58:39 -0300

    refactoring sensors

--- internal/sensor.go
+++ internal/sensor.go
@@ -1,55 +1,28 @@
 package processor
 
 import (
-	"math/rand"
+	//"math/rand"
 	//"time"
 	//"pkg/protocol"
 	//"fmt"
 )
 
-type CoreSensor struct {
-	ID          int
-	Temperature float64
-	Voltage     float64
-	Utilization float64
-	Frequency   float64
+type NodeSystemSensor struct {
+	ID          		int
 
-	BaseFrequency float64
-	MaxFrequency  float64
-	MinVoltage    float64
-	MaxVoltage    float64
-}
+	CurrentTemp 		float64
+	CurrentStress     	float64
+	CurrentPower 		float64
+	CurrentLatency   	float64
 
-func NewCoreSensor(id int) *CoreSensor {
-	return &CoreSensor{
-		ID:          id,
-		Temperature: 	35.0,
-		Voltage:     	0.9,
-		Utilization: 	0.0,
-		Frequency:   	3.600,
+	InputThroughput 	float64
+	InputInterrupt 		float64
+	HVACCoolingLevel 	float64
 
-		BaseFrequency: 	3.600,
-		MaxFrequency:  	5.200,
-		MinVoltage:    	0.9,
-		MaxVoltage:    	1.3,
-	}
-}
-
-func (c *CoreSensor) tickUpdate() {
-
-	c.Utilization += (rand.Float64() * 10) - 5
-
-	if c.Utilization > 100.0 {
-		c.Utilization = 100.0
-	}
-
-	if c.Utilization < 0.0 {
-		c.Utilization = 0.0
-	}
-
-	// Temperature
-
-	// Voltage
-
-	// Frequency
+	BasePower 			float64
+	MaxPower			float64
+	ThermalMass			float64
+	AmbientTemp			float64
+	NetworkCap			float64
+	BaseLatency			float64
 }
