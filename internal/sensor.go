1fa206f0d855684a80df53d23972e6625fdaee52
21 <victorsouzam74@gmail.com>
2026-03-20 13:12:54 -0300

    set new sensor base values

--- internal/sensor.go
+++ internal/sensor.go
@@ -16,7 +16,7 @@ type NodeSystemSensor struct {
 	CurrentLatency   	float64
 
 	InputThroughput 	float64
-	InputInterrupt 		float64
+	InputInterrupt 		int
 	HVACCoolingLevel 	float64
 
 	BasePower 			float64
@@ -25,4 +25,26 @@ type NodeSystemSensor struct {
 	AmbientTemp			float64
 	NetworkCap			float64
 	BaseLatency			float64
+}
+
+func NewNode(id int) *NodeSystemSensor{
+	return &NodeSystemSensor{
+		ID: 				id,
+		
+		CurrentTemp: 		22.0,
+		CurrentStress:     	0.0,
+		CurrentPower: 		250.0,
+		CurrentLatency:   	5.0,
+
+		InputThroughput: 	0.2,
+		InputInterrupt: 	5000,	
+		HVACCoolingLevel:	0.3,
+
+		BasePower: 			150.0,
+		MaxPower:			650.0,
+		ThermalMass:		50.0,	
+		AmbientTemp:		20.0,	
+		NetworkCap:			9.0,
+		BaseLatency:		2.0,	
+	}
 }
