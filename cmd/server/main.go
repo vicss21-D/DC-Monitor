d5551c95fc1b8252ec0a38b624e2c0d9d527cbe8
21 <victorsouzam74@gmail.com>
2026-03-28 10:19:03 -0300

    create server body

---
+++ cmd/server/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	"dc-monitor/pkg/protocol"
+)
+
+const (
+	Port        = ":9000"
+	WorkerCount = 10    
+	BufferSize  = 10000 
+)
+
+func main() {
+	// 1. Configuração do Socket UDP
+	addr, err := net.ResolveUDPAddr("udp", Port)
+	if err != nil {
+		log.Fatalf("Erro ao resolver endereço: %v", err)
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatalf("Erro ao iniciar servidor UDP: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Printf("Servidor Central Iniciado na porta %s\n", Port)
+	fmt.Printf("Iniciando pool com %d Workers...\n", WorkerCount)
+
+	// 2. A "Esteira Rolante" (Canal Bufferizado)
+	// Esse canal vai transportar as fatias de bytes cruas vindas da rede
+	packetQueue := make(chan []byte, BufferSize)
+
+	// O semáforo para garantir que os workers iniciem corretamente
+	var wg sync.WaitGroup
+
+	// 3. Contratando os Operários (Instanciando os Workers)
+	for i := 1; i <= WorkerCount; i++ {
+		wg.Add(1)
+		// Lançamos cada worker em sua própria Goroutine
+		go worker(i, packetQueue, &wg) 
+	}
+
+	networkBuffer := make([]byte, 2048)
+
+	fmt.Println("Aguardando telemetria dos nós...")
+
+	for {
+
+		n, _, err := conn.ReadFromUDP(networkBuffer)
+		if err != nil {
+			log.Printf("Erro de leitura UDP: %v", err)
+			continue
+		}
+
+		dataCopy := make([]byte, n)
+		copy(dataCopy, networkBuffer[:n])
+
+		packetQueue <- dataCopy
+	}
+}
+
+func worker(id int, queue <-chan []byte, wg *sync.WaitGroup) {
+	defer wg.Done() 
+
+	for rawData := range queue {
+		var packet protocol.TelemetryPacket
+
+		err := json.Unmarshal(rawData, &packet)
+		if err != nil {
+			log.Printf("Worker %d falhou ao decodificar JSON: %v", id, err)
+			continue
+		}
+
+		// ==========================================
+		// LB
+		// ==========================================
+		
+		if packet.CurrentState == protocol.StateCriticalLoad {
+			fmt.Printf("[ALERTA CRÍTICO - Tratado pelo Worker %d] Nó %d atingiu %.2f°C | Stress: %.2f%%\n",
+				id, packet.ID, packet.Temperature, packet.Stress)
+			
+		} else if packet.CurrentState == protocol.StateHighLoad {
+			//fmt.Printf("[AVISO] Nó %d em Estado de Alta Carga.\n", packet.NodeID)
+		}
+	}
+}
