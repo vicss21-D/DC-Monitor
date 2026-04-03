75d6f027b03859d9c0cbac2f61ac11d07ddde3ab
21 <victorsouzam74@gmail.com>
2026-04-03 13:51:04 -0300

    sort sensor functions

--- internal/sensor.go
+++ internal/sensor.go
@@ -67,80 +67,100 @@ func NewNode(id int) *NodeSystemSensor{
 	}
 }
 
-func (n *NodeSystemSensor) Tick() {
+// Tick orquestra o ciclo de vida do sensor a cada iteração
 
+func (n *NodeSystemSensor) Tick() {
 	n.TickCount++
 
-	if n.TickCount % 1000 == 0 && n.State != protocol.StateCriticalLoad {
-	
-		chance := rand.Intn(100) 
+	n.simulateStateAnomalies()
+	n.generateNetworkTraffic()
+	n.computeSystemStress()
+	n.computeNetworkLatency()
+	n.computePowerConsumption()
+	n.updateThermodynamics()
+}
 
-		if chance <= 1 {
-			
-			n.State = protocol.StateCriticalLoad
-		} else if chance <= 6 { 
-			
-			n.State = protocol.StateHighLoad
-		} else {
-			
-			n.State = protocol.StateNormalLoad
-		}
+// Injeta falhas e picos de carga baseados em probabilidade
+
+func (n *NodeSystemSensor) simulateStateAnomalies() {
+	if n.TickCount%1000 != 0 || n.State == protocol.StateCriticalLoad {
+		return
 	}
 
+	chance := rand.Intn(100)
+	if chance <= 1 {
+		n.State = protocol.StateCriticalLoad
+	} else if chance <= 6 {
+		n.State = protocol.StateHighLoad
+	} else {
+		n.State = protocol.StateNormalLoad
+	}
+}
+
+// Simula o tráfego e as interrupções de hardware com base no estado atual
+
+func (n *NodeSystemSensor) generateNetworkTraffic() {
 	switch n.State {
-
-		case protocol.StateNormalLoad:
-			
-			n.InputThroughput = 0.5 + (rand.Float64() * 0.2 - 0.1)
-		
-			n.InputInterrupts = 5000.0 + (rand.Float64() * 1000 - 500)
-
-		case protocol.StateHighLoad:
-			
-			n.InputThroughput = 8.5 + (rand.Float64() * 1.0 - 0.5)
-			
-			n.InputInterrupts = 60000.0 + (rand.Float64() * 5000 - 2500)
-
-		case protocol.StateCriticalLoad:
-			
-			n.InputThroughput = 0.1 + (rand.Float64() * 0.05) 
-			n.InputInterrupts = 1200000.0 + (rand.Float64() * 100000 - 50000)
-		}
-
-	if n.InputThroughput < 0 { 
-		n.InputThroughput = 0.01 
+	case protocol.StateNormalLoad:
+		n.InputThroughput = 0.5 + (rand.Float64()*0.2 - 0.1)
+		n.InputInterrupts = 5000.0 + (rand.Float64()*1000 - 500)
+	case protocol.StateHighLoad:
+		n.InputThroughput = 8.5 + (rand.Float64()*1.0 - 0.5)
+		n.InputInterrupts = 60000.0 + (rand.Float64()*5000 - 2500)
+	case protocol.StateCriticalLoad:
+		n.InputThroughput = 0.1 + (rand.Float64() * 0.05)
+		n.InputInterrupts = 1200000.0 + (rand.Float64()*100000 - 50000)
 	}
 
+	if n.InputThroughput < 0 {
+		n.InputThroughput = 0.01
+	}
+}
+
+// Calcula a carga do sistema com base nas interrupções e tráfego de rede
+
+func (n *NodeSystemSensor) computeSystemStress() {
 	weightThroughput := (n.InputThroughput / n.NetworkCap) * 60.0
 	weightInterrupts := (n.InputInterrupts / 1000000.0) * 100.0
-	
+
 	n.CurrentStress = weightThroughput + weightInterrupts
 	if n.CurrentStress > 100.0 {
 		n.CurrentStress = 100.0
 	}
 
 	availableCPUForData := 100.0 - weightInterrupts
-	if availableCPUForData < 0 { availableCPUForData = 0 }
-	
-	actualThroughput := n.InputThroughput * (availableCPUForData / 100.0)
-	if actualThroughput > n.NetworkCap { actualThroughput = n.NetworkCap }
+	if availableCPUForData < 0 {
+		availableCPUForData = 0
+	}
 
-	utilizationFraction := n.CurrentStress / 100.0 
-	
+	actualThroughput := n.InputThroughput * (availableCPUForData / 100.0)
+	if actualThroughput > n.NetworkCap {
+		actualThroughput = n.NetworkCap
+	}
+}
+
+func (n *NodeSystemSensor) computeNetworkLatency() {
+	utilizationFraction := n.CurrentStress / 100.0
+
 	if utilizationFraction >= 0.99 {
-		n.CurrentLatency = 5000.0 
+		n.CurrentLatency = 5000.0
 	} else {
 		n.CurrentLatency = n.BaseLatency / (1.0 - utilizationFraction)
 	}
 	n.CurrentLatency += rand.Float64() * 0.5
+}
 
+func (n *NodeSystemSensor) computePowerConsumption() {
+	utilizationFraction := n.CurrentStress / 100.0
 	dynamicPower := (n.MaxPower - n.BasePower) * utilizationFraction
+	
 	n.CurrentPower = n.BasePower + dynamicPower + (rand.Float64() * 5.0)
+}
 
-	heatGenerated := n.CurrentPower * 0.0005 
-	
+func (n *NodeSystemSensor) updateThermodynamics() {
+	heatGenerated := n.CurrentPower * 0.0005
 	heatDissipated := n.HVACCoolingLevel * 0.002 * (n.CurrentTemp - n.AmbientTemp)
-	
+
 	tempVariation := (heatGenerated - heatDissipated) / n.ThermalMass
 	n.CurrentTemp += tempVariation
 
